56a6464b3da30a2bb87e012a4e33f346a896c731
Brent-the-carpenter <beh337@gmail.com>
2024-02-02 10:56:25 -0500

    first commit/ changed set up for new project

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# Nav-Bar-tool
+# Image-slider
 
-This is a simple nav-bar that i am going to publish to the Npm registry for use later in my future projects to save time.
+This is a simple image-slider that I am going to publish to the Npm registry for use later in my future projects to save time.
--- src/index.js
+++ src/index.js
@@ -1,32 +1,8 @@
-import "./style.css";
-import Icon from "./menu.svg";
+import "./style.css"
 
-function addNavBar(elementToAppendTo, numberOfItems) {
-  const Nav = document.createElement("nav");
-  const Ul = document.createElement("ul");
-  const Li = document.createElement("li");
-  const myIcon = new Image();
-  myIcon.src = Icon;
-  let i = 1;
-  let addNavToThis = document.querySelector(elementToAppendTo);
-  Nav.setAttribute("id", "nav");
 
-  addNavToThis.appendChild(Nav);
-  Nav.appendChild(Ul);
-  Ul.appendChild(Li);
-  Li.appendChild(myIcon);
-  Li.appendChild(document.createElement("ul")).setAttribute(
-    "class",
-    "drop-menu"
-  );
-  while (i <= numberOfItems) {
-    let menu = document.querySelector(".drop-menu");
-    menu
-      .appendChild(document.createElement("li"))
-      .setAttribute("id", `menu-item-${i}`);
-    document.querySelector(`#menu-item-${i}`).textContent = `item ${i}`;
-    i++;
-  }
+function imageSlider ( elementTOAppendTO,imageArray){
+ let imageContainer = document.querySelector(elementTOAppendTO);
+
+ imageContainer.
 }
-
-export { addNavBar };
